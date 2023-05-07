ad7551a164415c7d542f10bf18701ffd1e88b9cb
damieroliverroot <daffarres@gmail.com>
2023-05-07 21:17:39 +0700

    Initial commit

---
+++ src/main/kotlin/com/daffa/Application.kt
@@ -0,0 +1,17 @@
+package com.daffa
+
+import io.ktor.server.application.*
+import com.daffa.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureMonitoring()
+    configureSerialization()
+//    configureSockets()
+    configureRouting()
+}
